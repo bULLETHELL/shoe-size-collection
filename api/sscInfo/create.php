6b4c227a36e793a8a1894915633efdd021640fcb
bULLETHELL <polyswing7@hotmail.com>
2019-08-02 08:33:55 +0200

    create problems

--- api/sscInfo/create.php
+++ api/sscInfo/create.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Max-Age: 3600');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested_With');
@@ -8,9 +8,11 @@ header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 include_once '../config/database.php';
 include_once '../objects/sscInfo.php';
 
+echo json_encode(array("message" => "Create running."));
+
 $database = new Database();
 $dbcon = $database->getCon();
-$sscInfo = new sscInfo();
+$sscInfo = new sscInfo($dbcon);
 $data = json_decode(file_get_contents("php://input"));
 
 if(
